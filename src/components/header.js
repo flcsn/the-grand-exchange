cbc592fe423e46ca86fb02a6d661c67d955dedc4
flcsn <flacson98@gmail.com>
2022-02-15 18:29:36 +0800

    added header and footer to product page; clicking logo now brings user to main page

--- src/components/header.js
+++ src/components/header.js
@@ -10,7 +10,9 @@ const Header = () => {
 
   return (
     <div className='header'>
-      <img src={logo} alt='sample logo'/>
+      <Link to='/main'>
+        <img src={logo} alt='sample logo'/>
+      </Link>
       <form onSubmit={(event) => handleClick(event)}>
         <input type='text' placeholder='Search for an item' />
         <button type='submit'>Go!</button>
