bc6496557f90c0408a82100f29e6eabf5041a2ed
flcsn <flacson98@gmail.com>
2022-02-16 20:25:44 +0800

    implemented login functionality

---
+++ backend/controllers/login.js
@@ -0,0 +1,37 @@
+const loginRouter = require('express').Router()
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../utils/config')
+const User = require('../models/user')
+
+loginRouter.post('/', async (req, res) => {
+  const user = await User.findOne({ username: req.body.username }).select('+passwordHash')
+
+  if (!user) {
+    return res.status(404).json({
+      error: 'user not found'
+    })
+  }
+
+  const passwordCorrect = await bcrypt.compare(req.body.password, user.passwordHash)
+
+  if (!passwordCorrect) {
+    return res.status(401).json({
+      error: 'incorrect password'
+    })
+  }
+
+  const userForToken = {
+    id: user.id,
+    username: user.username
+  }
+
+  const token = jwt.sign(userForToken, JWT_SECRET)
+
+  return res.status(200).json({
+    token,
+    username: user.username
+  })
+})
+
+module.exports = loginRouter
--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,14 @@ const { PORT } = require('./utils/config')
 const app = express()
 
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 app.use(express.json())
 app.use(cors())
 app.use(helmet())
 
 app.use('/users', usersRouter)
+app.use('/login', loginRouter)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
