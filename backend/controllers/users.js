d737f2c08d334013fbe3f72b41c7fd5d5aa289ba
flcsn <flacson98@gmail.com>
2022-02-16 19:09:05 +0800

    installed bcrypt and added route for creating new users

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,9 +1,22 @@
 const usersRouter = require('express').Router()
 const User = require('../models/user')
+const bcrypt = require('bcrypt')
 
 usersRouter.get('/', async (req, res) => {
   const users = await User.find({})
   return res.json(users)
+})
+
+usersRouter.post('/', async (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    password: bcrypt.hash(req.body.password, 10),
+    emailAddress: req.body.emailAddress
+  })
+
+  const savedUser = await user.save()
+
+  res.json(savedUser)
 })
 
 module.exports = usersRouter
