b7dd5c84553a3d09af2bb4a75cee593868fbfaf8
flcsn <flacson98@gmail.com>
2022-02-17 17:41:01 +0800

    created products router

---
+++ backend/controllers/products.js
@@ -0,0 +1,46 @@
+const productsRouter = require('express').Router()
+const Product = require('../models/product')
+
+productsRouter.get('/', async (req, res) => {
+  const products = await Product.find({})
+  return res.json(products)
+})
+
+productsRouter.get('/:id', async (req, res) => {
+  const product = await Product.findById(req.params.id)
+  if (!product)
+    return res.status(404).json({
+      error: 'product not found'
+    })
+
+  return res.json(product)
+})
+
+productsRouter.post('/', async (req, res) => {
+  const newProduct = new Product({
+    title: req.body.title,
+    description: req.body.description,
+    stock: req.body.stock,
+    price: req.body.price
+  })
+
+  try {
+    const savedProduct = await newProduct.save()
+    return res.status(201).json(savedProduct)
+  } catch (e) {
+    return res.status(400).json({
+      error: e.message
+    })
+  }
+})
+
+productsRouter.delete('/:id', async (req, res) => {
+  const product = await Product.findById(req.params.id)
+  if (!product)
+    return res.status(404).end()
+
+  await Product.delete(product)
+  return res.status(204).end()
+})
+
+module.exports = productsRouter
