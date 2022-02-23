dffa8bca4053536992d621f76cc23a3643fdcc15
flcsn <flacson98@gmail.com>
2022-02-23 20:12:07 +0800

    added styles to product item

--- frontend/src/components/product-item.js
+++ frontend/src/components/product-item.js
@@ -6,12 +6,16 @@ const ProductItem = ({ product }) => {
   const base64Image = btoa(String.fromCharCode(...new Uint8Array(product.image.data.data)))
 
   return (
-    <div>
+    <div className='product-item'>
       <img src={`data:${product.image.contentType};base64,
                      ${base64Image}`} style={{ height: '200px', width: '200px' }}/>
       <h2>{product.title}</h2>
-      <p>{product.price}</p>
-      <Link to='/products/1'>See More</Link>
+      <p>PhP {product.price}</p>
+      <Link to='/products/1'>
+        <div className='product-item-link'>
+          See More
+        </div>
+      </Link>
     </div>
   )
 }
