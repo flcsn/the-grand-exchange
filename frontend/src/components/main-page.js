80818ba0cfd8e378f03ee59b5f54a252df1c3d37
flcsn <flacson98@gmail.com>
2022-02-23 20:15:31 +0800

    added product list component and styles

--- frontend/src/components/main-page.js
+++ frontend/src/components/main-page.js
@@ -1,16 +1,14 @@
 import React from 'react'
 
 import Header from './header'
-import ProductItem from './product-item'
 import Footer from './footer'
+import ProductList from './product-list'
 
 const MainPage = ({ products }) => {
   return (
     <div>
       <Header />
-      {products.map(product =>
-        <ProductItem key={product.id} product={product} />)
-      }
+      <ProductList products={products} />
       <Footer />
     </div>
   )
