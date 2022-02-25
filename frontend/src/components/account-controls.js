6358ac058467f7d2faadf2c3166d6a0cba690194
flcsn <flacson98@gmail.com>
2022-02-25 20:13:26 +0800

    clicking outside dropdown box now closes the menu

--- frontend/src/components/account-controls.js
+++ frontend/src/components/account-controls.js
@@ -1,8 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import UserDropdown from './user-dropdown'
 
 const AccountControls = ({ user }) => {
   const [displayDropdown, setDisplayDropdown] = useState(false)
+  const dropdownRef = useRef()
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (!displayDropdown) return
+      if (!dropdownRef.current.contains(event.target)) {
+        setDisplayDropdown(false)
+      }
+    }
+    document.addEventListener('mousedown', handleClickOutside)
+  }, [dropdownRef, displayDropdown])
 
   return (
     <div className='header-account-controls'>
@@ -12,7 +23,7 @@ const AccountControls = ({ user }) => {
       >
         {user.username}
       </span>
-      <UserDropdown user={user} displayDropdown={displayDropdown} />
+      <UserDropdown user={user} displayDropdown={displayDropdown} ref={dropdownRef} />
     </div>
   )
 }
--- frontend/src/components/user-dropdown.js
+++ frontend/src/components/user-dropdown.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { logout } from '../reducers/userReducer'
 
-const UserDropdown = ({ user, displayDropdown }) => {
+const UserDropdown = forwardRef(({ user, displayDropdown }, ref) => {
   const dispatch = useDispatch()
 
   const className = `user-dropdown-menu ${displayDropdown ? 'active' : ''}`
@@ -14,12 +14,14 @@ const UserDropdown = ({ user, displayDropdown }) => {
   }
 
   return (
-    <div className={className}>
+    <div className={className} ref={ref}>
       <Link className='transparent-gray-link' to={`/user/${user.username}/products`}>My Products</Link>
       <Link className='transparent-gray-link' to='/main'>Account Settings</Link>
       <button className='logout-btn' onClick={handleLogout}>Log Out</button>
     </div>
   )
-}
+})
+
+UserDropdown.displayName = 'UserDropdown'
 
 export default UserDropdown
