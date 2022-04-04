3467003d3b7edfbc0be6e94b18ba17c2f6407019
flcsn <flacson98@gmail.com>
2022-04-04 14:54:20 +0800

    added conditional style rendering for success/error notifications

--- frontend/src/components/notification.js
+++ frontend/src/components/notification.js
@@ -2,14 +2,22 @@ import React from 'react'
 import { HiOutlineCheckCircle, HiOutlineXCircle } from 'react-icons/hi'
 
 const Notification = ({ notification }) => {
-  if (notification === '')
+  if (notification.message === '')
     return null
 
+  const isSuccess = notification.type === 'success'
+
+  const className = isSuccess
+    ? 'notification success'
+    : 'notification error'
+
   return (
-    <div className='notification error'>
-      {notification}
-      <HiOutlineCheckCircle className='success-icon'/>
-      <HiOutlineXCircle className='error-icon' />
+    <div className={className}>
+      {notification.message}
+      { isSuccess
+        ? <HiOutlineCheckCircle className='success-icon'/>
+        : <HiOutlineXCircle className='error-icon' />
+      }
     </div>
   )
 }
--- frontend/src/reducers/notificationReducer.js
+++ frontend/src/reducers/notificationReducer.js
@@ -1,25 +1,33 @@
-const notificationReducer = (state = '', action) => {
+const baseState = {
+  type: '',
+  message: ''
+}
+
+const notificationReducer = (state = baseState, action) => {
   switch (action.type) {
     case 'SET_NOTIFICATION':
       return action.data
     case 'CLEAR_NOTIFICATION':
-      return ''
+      return baseState
     default:
       return state
   }
 }
 
-export const setNotification = (message) => {
+export const setNotification = (type, message) => {
   return async dispatch => {
     dispatch({
       type: 'SET_NOTIFICATION',
-      data: message
-    })/*
+      data: {
+        type,
+        message
+      }
+    })
     setTimeout(() => {
       dispatch({
         type: 'CLEAR_NOTIFICATION'
       })
-    }, 3000)*/
+    }, 3000)
   }
 }
 
