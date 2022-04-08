a307a92d91dcbb78a78b68d9d0b0b0d11eca0a5c
flcsn <flacson98@gmail.com>
2022-04-08 16:58:13 +0800

    search results now displays number of products found

--- frontend/src/components/search-results.js
+++ frontend/src/components/search-results.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 import Header from './header'
 import ProductList from './product-list'
 import Footer from './footer'
 
 const SearchResults = ({ products }) => {
+  const location = useLocation()
+  const title = new URLSearchParams(location.search).get('title')
+  console.log('querying for', title)
+
   return (
     <div>
       <Header />
+      <div className='search-status'>
+        <p>{products.length} { products.length === 1 ? 'product' : 'products'} found for &apos;{title}&apos;</p>
+      </div>
       <ProductList products={products}/>
       <Footer />
     </div>
