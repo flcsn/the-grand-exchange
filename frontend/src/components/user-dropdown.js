3612f48668d29172c2f7fcf46d8f911bd308bf4a
flcsn <flacson98@gmail.com>
2022-04-12 11:24:21 +0800

    user dropdown now shows current funds

--- frontend/src/components/user-dropdown.js
+++ frontend/src/components/user-dropdown.js
@@ -2,6 +2,7 @@ import React, { forwardRef } from 'react'
 import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { logout } from '../reducers/userReducer'
+import { AiFillPlusCircle } from 'react-icons/ai'
 
 const UserDropdown = forwardRef(({ user, displayDropdown }, ref) => {
   const dispatch = useDispatch()
@@ -13,11 +14,21 @@ const UserDropdown = forwardRef(({ user, displayDropdown }, ref) => {
     dispatch(logout())
   }
 
+  const handleClick = () => {
+    console.log('button clicked')
+  }
+
   return (
     <div className={className} ref={ref}>
       <p className='user-dropdown-menu-username'>
         {user.username}
       </p>
+      <div className='user-dropdown-menu-funds'>
+        <p>Funds: <strong>{user.funds}</strong></p>
+        <div onClick={handleClick}>
+          <AiFillPlusCircle />
+        </div>
+      </div>
       <Link className='transparent-gray-link' to={`/user/${user.username}/products`}>My Products</Link>
       <Link className='transparent-gray-link' to='/main'>Account Settings</Link>
       <button className='logout-btn' onClick={handleLogout}>Log Out</button>
