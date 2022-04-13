da30ef86e023b92e73acbd1defe64c42027ad5eb
flcsn <flacson98@gmail.com>
2022-04-13 16:22:08 +0800

    added basic styles to wallet form

--- frontend/src/components/wallet-form.js
+++ frontend/src/components/wallet-form.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { closeModal } from '../reducers/modalReducer'
 
 const WalletForm = () => {
+  const user = useSelector(state => state.user)
   const dispatch = useDispatch()
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log('added to wallet', event.target.funds.value)
+  }
+
   return (
-    <div className='modal-form-container'>
+    <div id='wallet-modal-box' className='modal-form-container-sm'>
       <h2>Add Funds</h2>
       <button
         className='close-modal-form-btn'
@@ -14,7 +20,15 @@ const WalletForm = () => {
       >
         &times;
       </button>
-      <p>Hello!</p>
+      <p>You currently have: <strong>{user.funds}</strong></p>
+      <form
+        className='wallet-form'
+        onSubmit={(event) => handleSubmit(event)}
+      >
+        <label>How much would you like to add?</label>
+        <input name='funds' type='number' min='1' required />
+        <button type='submit'>Add</button>
+      </form>
     </div>
   )
 }
